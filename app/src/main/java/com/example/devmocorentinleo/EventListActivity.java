77a432b44c7aeb3aef0ee98641b3bd4eb67bf660
Corentin <coco@clementmacfarlane.fr>
2023-03-16 11:38:20 +0100

    Implementation Recycler View

--- app/src/main/java/com/example/devmocorentinleo/EventListActivity.java
+++ app/src/main/java/com/example/devmocorentinleo/EventListActivity.java
@@ -1,4 +1,34 @@
 package com.example.devmocorentinleo;
 
-public class EventListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.LinkedList;
+
+public class EventListActivity extends AppCompatActivity {
+    private final LinkedList<String> mEventList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private EventListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventlist);
+        for (int i = 0; i < 20; i++) {
+            mEventList.addLast("Word " + i);
+        }
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new EventListAdapter(this, mEventList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
 }
